510c0260f671d2d57e28a71da1b263fa6f0d77a0
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-07-09 12:57:56 +0530

    nav scroll up animation

---
+++ script/header.js
@@ -0,0 +1,18 @@
+// var header = document.querySelector(".header");
+// var nav = document.querySelector(".nav");
+// var icon = document.querySelector(".icon");
+
+// window.onscroll = function(){scrollFunctionHeader()};
+
+// const scrollFunctionHeader = () => {
+//     if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+//         header.style.height = "0";
+//         nav.style.fontSize = "0";
+//         icon.style.fontSize = "0";
+//     }
+//     else {
+//         header.style.height = "70px";
+//         nav.style.fontSize = "1.33rem";
+//         icon.style.fontSize = "3rem";
+//     }
+// }
--- script/scroll-up.js
+++ script/scroll-up.js
@@ -1,7 +1,26 @@
-var scroller = document.querySelector(".scroll-up");
+
 
 window.onscroll = function(){scrollFunction()};
 
+
+var header = document.querySelector(".header");
+var nav = document.querySelector(".nav");
+var icon = document.querySelector(".icon");
+const scrollFunctionHeader = () => {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        header.style.height = "0";
+        nav.style.fontSize = "0";
+        icon.style.fontSize = "0";
+    }
+    else {
+        header.style.height = "70px";
+        nav.style.fontSize = "1.33rem";
+        icon.style.fontSize = "3rem";
+    }
+}
+
+
+var scroller = document.querySelector(".scroll-up");
 const scrollFunction = () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         scroller.style.display = "grid";
@@ -9,4 +28,5 @@ const scrollFunction = () => {
     else {
         scroller.style.display = "none";
     }
+    scrollFunctionHeader();
 }
