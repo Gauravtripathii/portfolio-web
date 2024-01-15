25dc12fa2d1879c42cd421f5c3ad3f1fbc309f88
gauravtripathii <gauravtripathii>
2024-01-15 15:32:53 +0530

    nav responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         // h-screen w-9/12 overflow-y-scroll scroll-smooth
-        <div className="overflow-y-scroll scrollbar-hidden w-9/12">
+        <div className="overflow-y-scroll scrollbar-hidden xl:w-9/12">
             <div className="h-screen"></div>
             <div className="h-screen bg-green-50"></div>
             <div className="h-screen"></div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ function Nav() {
     <div className="border-b w-full p-3 flex flex-col items-center justify-center xl:w-3/12 xl:h-screen xl:border-b-0 xl:border-r xl:gap-52">
         <Logo />
         {/* flex flex-col text-3xl font-baskerville text-white gap-5 */}
-        <nav className="flex gap-5 lg:gap-10 justify-center items-center text-2xl font-baskerville text-white xl:flex-col">
+        <nav className="flex gap-5 lg:gap-10 justify-center items-center xl:text-2xl font-baskerville text-white xl:flex-col">
             <a href="#home" className="hover:text-redish active:text-redish">Home</a>
             <a href="#about" className="hover:text-redish active:text-redish">About Me</a>
             <a href="#projects" className="hover:text-redish active:text-redish">Projects</a>
