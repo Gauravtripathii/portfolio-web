adde48ddae4ac964b602e7293b3aa372ad9a178d
gauravtripathii <gauravtripathii>
2024-01-22 16:40:21 +0530

    Home responsive fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
   return (
     <div className="overflow-y-scroll scrollbar-hidden xl:w-10/12 scroll-smooth">
         {/* HOME SECTION */}
-      <div className="h-[75svh] xl:h-[100svh] flex flex-col items-center justify-between gap-10 lg:gap-20 xl:justify-between xl:pb-10 xl:pt-20 2xl:pb-24 2xl:pt-28" id="home">
+      <div className="h-[75svh] flex flex-col justify-between items-center xl:h-[100svh] xl:relative xl:justify-center" id="home">
         {/* main */}
         <div className="w-full flex flex-col items-center p-5 gap-5 lg:gap-10 xl:w-3/4 xl:gap-5 2xl:gap-10">
           {/* profile img part */}
@@ -45,7 +45,7 @@ function Home() {
           </div>
         </div>
         {/* SCROLL DOWN WIDGET */}
-        <a href="#about" className="flex flex-col items-center gap-1">
+        <a href="#about" className="flex flex-col items-center gap-1 xl:absolute xl:bottom-0">
             <p className="text-white font-mono">Scroll Down</p>
             <div className="w-7 h-10 border rounded-full p-1 relative">
                 <span className="block w-2 h-2 bg-white rounded-full absolute left-1/2 -translate-x-1/2 scroll-animate"></span>
